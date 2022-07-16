9f1c57d8d9bbcf5ff2ecc5f1fd6bbd668bfd24bf
YUVRAJ6336 <102599905+YUVRAJ6336@users.noreply.github.com>
2022-07-16 18:24:07 +0530

    learned turtle
    variables- numbers, strings
    functions-initialization and calling them

---
+++ turtle.py
@@ -0,0 +1,27 @@
+import turtle
+
+a=5
+print(a)
+b="hello"
+print(b)
+credit_card=4242424242424242
+print(credit_card)
+
+
+qazi_turtle = turtle.Turtle()
+
+# square
+def square():
+ 
+  qazi_turtle.forward(100)
+  qazi_turtle.right(90)
+  qazi_turtle.forward(100)
+  qazi_turtle.right(90)
+  qazi_turtle.forward(100)
+  qazi_turtle.right(90)
+  qazi_turtle.forward(100)
+
+
+square()
+qazi_turtle.forward(100)
+square()
